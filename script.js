57bcaba01807eefd3fabfb3c5397cd19dd27b296
Ankit Jangir <61105805+ankitweb24@users.noreply.github.com>
2020-03-29 15:52:37 +0530

    darkseries
    please pubshid this website

---
+++ script.js
@@ -0,0 +1,77 @@
+var deg=6;
+var hr=document.getElementById('hr')
+var mm=document.getElementById('mn');
+var ss=document.getElementById('sc');
+ setInterval(() => {
+    var day=new Date();
+    var hh=day.getHours()*30;
+    var mm=day.getMinutes()*deg;
+    var ss=day.getSeconds()*deg;
+    hr.style.transform=`rotateZ(${hh+(mm/12)}deg)`;
+    mn.style.transform=`rotateZ(${mm}deg)`;
+    sc.style.transform=`rotateZ(${ss}deg)`; 
+ });
+ window.onscroll = function () {
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        zoomEnabled: true,
+        theme: "dark2",
+        title:{
+            text: "AJ_WEBKIT COMPANY"
+        },
+        axisX: {
+            title:"growth our project",
+            suffix: "%",
+            minimum: 0,
+            maximum: 61,
+            gridThickness: 1
+        },
+        axisY:{
+            title: "dark devlopmentr",
+            suffix: "mn"
+        },
+        data: [{
+            type: "bubble",
+            toolTipContent: "<b>{name}</b><br/>Employment: {x}% <br/> Agri-Land: {y}mn sq. km<br/> Population: {z}mn",
+            dataPoints: [
+                { x: 89.6, y: 5.225, z:1347, name:"india" },
+                { x: 3.3, y: 4.17, z:21.49, name:"Australia" },
+                { x: 1.5, y: 4.043, z:304.09, name:"US" },
+                { x: 17.4, y: 2.647, z:2.64, name:"Brazil" },
+                { x: 8.6, y: 2.154, z:141.95, name:"Russia" },
+                { x: 52.98, y: 1.797, z:1190.86, name:"India" },
+                { x: 4.3, y: 1.735, z:26.16, name:"Saudi Arabia" },
+                { x: 1.21, y: 1.41, z:39.71, name:"Argentina" },
+                { x: 5.7, y: .993, z:48.79, name:"SA" },
+                { x: 13.1, y: 1.02, z:110.42, name:"Mexico" },
+                { x: 2.4, y: .676, z:33.31, name:"Canada" },
+                { x: 2.8, y: .293, z:64.37, name:"France" },
+                { x: 3.8, y: .46, z:127.70, name:"Japan" },
+                { x: 40.3, y: .52, z:234.95, name:"Indonesia" },
+                { x: 42.3, y: .197, z:68.26, name:"Thailand" },
+                { x: 31.6, y: .35, z:78.12, name:"Egypt" },
+                { x: 1.1, y: .176, z:61.39, name:"U.K" },
+                { x: 3.7, y: .144, z:59.83, name:"Italy" },
+                { x: 1.8, y: .169, z:82.11, name:"Germany" }
+            ]
+        }]
+    });
+    chart.render();
+    
+    }
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows : true,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
